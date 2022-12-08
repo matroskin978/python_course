9c779da97722b373ede058332cbaf47799657ceb
Andrey Kudlay <matroskin8@i.ua>
2022-12-08 10:30:31 +0200

    lesson 4

---
+++ 4/4.py
@@ -0,0 +1,14 @@
+# block comment
+print('Hello, World!')
+print('Hello, World!!')  # inline comment
+
+''' One-line docstring '''
+""" One-line docstring """
+
+''' Multi-line
+docstring
+'''
+
+""" Multi-line
+docstring
+"""
