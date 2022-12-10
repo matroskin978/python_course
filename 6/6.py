ed70bf312eeac67a9743982d6286fd00befdabbf
Andrey Kudlay <matroskin8@i.ua>
2022-12-10 21:24:18 +0200

    6

---
+++ 6/6.py
@@ -0,0 +1,36 @@
+# Python поддерживает целые числа, числа с плавающей запятой и комплексные числа.
+num1 = 10
+num2 = 5.4
+print(type(num1))
+print(type(num2))
+
+# С числами можно производить математические операции
+# http://pythonicway.com/python-operators
+print(20 + 4)
+print(20 - 4)
+print(20 * 4)
+print(20 / 4)
+print(20 % 4)
+print(21 % 4)
+print(21 // 4)
+print(20 ** 4)
+
+# Приоритет операторов
+print(5 + 3 * 2)
+print((5 + 3) * 2)
+
+# print(.1 + .2)
+#
+# from decimal import *
+# print(Decimal('.1') + Decimal('.2'))
+
+# from Python 3.6
+num3 = 1000000000
+num4 = 1_000_000_000
+print(num3)
+print(num4)
+
+# max number
+# print(1000**1000)
+num5 = 1000 ** 1000
+print(type(num5))
