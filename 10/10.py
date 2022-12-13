a03f61aa60d2cdc51c7b14825af388295f41b462
Andrey Kudlay <matroskin8@i.ua>
2022-12-13 21:29:25 +0200

    10

---
+++ 10/10.py
@@ -0,0 +1,36 @@
+# None
+_none = None
+print(type(_none))
+_none = 1 + 2
+print(type(_none))
+
+_true = True
+_false = False
+print(type(_true))
+print(type(_false))
+
+'''
+False equal:
+False
+0
+''
+None
+[]
+()
+{}
+'''
+
+num1 = 5.2
+num2 = 10
+print(type(num1), type(num2))
+num1 = int(num1)
+num2 = float(num2)
+print(type(num1), type(num2))
+print(num1, num2)
+
+print(bool(False))
+print(bool(0))
+print(bool(''))
+print(bool(None))
+print(bool(1))
+print(bool('Hi'))
