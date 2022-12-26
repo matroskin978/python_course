8e3d501f7204b8346d99a16b13f05ed3baf0cf1c
Andrey Kudlay <matroskin8@i.ua>
2022-12-26 21:49:52 +0200

    31_2

--- 31/31.py
+++ 31/31.py
@@ -25,6 +25,15 @@ red, black, *other_colors = colors
 print(red, black, other_colors)
 print(*colors)
 
+# распаковка последовательности без переменной может быть использована для функции range
+t1 = 1, 10
+# print(type(t1), t1)
+# print(*t1)
+# for i in range(*t1):
+#     print(i, end=' ')
+# print([*range(*t1)])
+# print(list(range(1, 10)))
+
 # Также можно распаковать только крайние элементы
 red, *other_colors, green = colors
 print(red, green, other_colors)
@@ -46,7 +55,7 @@ l3 = l1 + l2
 l3 = [*l1, *l2]
 print(l3)
 
-# Распаковка словаря с помощью **
+# Распаковка словаря в другой словарь с помощью **
 d1 = {'a': 1, 'b': 2, 'c': 3}
 d2 = {**d1}
 d2 = d1.copy()
@@ -54,3 +63,23 @@ d2 = d1
 print(d1 is d2)
 print(id(d1))
 print(id(d2))
+
+# Распаковка словаря
+colors_d = {'red': 'красный', 'black': 'черный', 'white': 'белый', 'green': 'зеленый'}
+# a, *b = colors_d  # colors_d.keys()
+# a, *b = colors_d.values()  # получим значения
+a, *b = colors_d.items()  # получим кортежи пар ключ:значение
+print(a, b)
+
+# Распаковка множеств
+s = {'красный', 'черный', 'белый', 'зеленый'}
+a, *b = s
+print(a, b)
+
+# С помощью *, можно не только распаковывать, но и объединять итерируемые объекты:
+s = {'one', 'two', 'three'}
+t = (4, 10)
+l = [True, False, 5.4]
+res = [*s, *range(*t), *t, *l]
+print(res)
+
